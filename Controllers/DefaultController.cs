1ac7fa366c5d654c91b565e349f294cf298945cf
art <partypeopleland@gmail.com>
2019-11-27 12:22:06 +0800

    加入MVC

---
+++ Controllers/DefaultController.cs
@@ -0,0 +1,14 @@
+﻿using Art.NetCore.SignalR.Model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Art.NetCore.SignalR.Controllers
+{
+    public class DefaultController : Controller
+    {
+        public IActionResult Index()
+        {
+            var user = new User() { Name = "Art" };
+            return View(model: user);
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -1,12 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.IO;
 using Art.NetCore.SignalR.Hub;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Hosting;
 
 namespace Art.NetCore.SignalR
@@ -17,26 +15,20 @@ namespace Art.NetCore.SignalR
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc(options => options.EnableEndpointRouting = false);
             services.AddSignalR();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.UseRouting();
-            
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapHub<ChatHub>("/chatHub");
-            });
+            if (env.IsDevelopment()) app.UseDeveloperExceptionPage();
             app.UseDefaultFiles();
             app.UseStaticFiles();
+            app.UseMvcWithDefaultRoute();
+            app.UseRouting();
+            app.UseEndpoints(endpoints => { endpoints.MapHub<ChatHub>("/chatHub"); });
+        
         }
     }
 }
